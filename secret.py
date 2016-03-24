b8cf54d8853a79889928624e46503690770fb853
Alex Ahn <alexahn@seoulchristian.org>
2016-03-24 23:18:33 +0900

    Add decrypt function

--- secret.py
+++ secret.py
@@ -12,3 +12,13 @@ def load_sheet(filename):
     with open(filename, "r") as f:
 		contents = f.read().splitlines()
 	return contents
+
+def decrypt(ciphertext, sheet):
+    plaintext = ''
+    for position, character in enumerate(ciphertext):
+        if character not in ALPHABET:
+            plaintext += character
+        else:
+            decrypted = (ALPHABET.index(character) - int(sheet[position])) % 26
+            plaintext += ALPHABET[decrypted]
+    return plaintext
