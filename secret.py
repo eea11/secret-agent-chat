31779bb2833c46b0d7da5224998e6b2c8251c557
Alex Ahn <alexahn@seoulchristian.org>
2016-03-24 23:20:29 +0900

    Add get_plain_text function

--- secret.py
+++ secret.py
@@ -13,12 +13,6 @@ def load_sheet(filename):
 		contents = f.read().splitlines()
 	return contents
 
-def decrypt(ciphertext, sheet):
-    plaintext = ''
-    for position, character in enumerate(ciphertext):
-        if character not in ALPHABET:
-            plaintext += character
-        else:
-            decrypted = (ALPHABET.index(character) - int(sheet[position])) % 26
-            plaintext += ALPHABET[decrypted]
-    return plaintext
+def get_plain_text():
+    plain_text = input('Please type your message ')
+    return plain_text.lower()
