c9a51363fa8eb300db4450564d0a29cb8dec27a7
Alex Ahn <alexahn@seoulchristian.org>
2016-03-24 23:21:32 +0900

    Create function for saving and loading message

--- secret.py
+++ secret.py
@@ -16,3 +16,12 @@ def load_sheet(filename):
 def get_plain_text():
     plain_text = input('Please type your message ')
     return plain_text.lower()
+
+def load_file(filename):
+    with open(filename, "r") as f:
+        contents = f.read()
+    return contents
+
+def save_file(filename, data):
+    with open(filename, 'w') as f:
+        f.write(data)
